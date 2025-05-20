2bb54669e98ab5a68d4dd0e8baa9066eac2abb29
veekshitha ms <veekshithams1@gmail.com>
2025-05-20 09:37:50 +0530

    Hello world

---
+++ App.java
@@ -0,0 +1,13 @@
+package Devops.prog10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
